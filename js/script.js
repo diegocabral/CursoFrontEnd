cb153fd9f71ce6e7b2f09de6009645d3f74bf16e
Diego Roberto Cabral <diegorobertocabral@gmail.com>
2018-06-22 18:22:34 -0300

    Envio
    Envio

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* alert("olá"); */
+
+/*
+var resposta = confirm("ola");
+console.log(resposta);
+*/
+
+/*
+const a = 10;
+const b = 33;
+
+function somar(a, b){
+    return a + b;
+}
+*/
+
+document.querySelector("#calcular").addEventListener("click", function(){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+
+    if(valorA.length > 0 && valorB.length > 0){
+    alert(parseInt(valorA) + parseInt(valorB));
+    }else{
+        alert("Digite os valores");
+    }
+});
